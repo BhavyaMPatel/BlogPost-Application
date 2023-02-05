8377073812aa218c8e6ef2872ff893b996c111f6
bhavyapatel <bhavyapatel4012@gmail.com>
2023-02-05 10:49:19 +0530

    FrontEnd Done Start Work With BackEnd

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   res.render("HomePage",{Post:blogs});
 });
 
-const blogs=[{cat:"game",date:"2023",auther:"Bhavya",title:"Hello HoneyBony",blog:"Hello HoneyBony"},{cat:"game",date:"2023",auther:"Bhavya",title:"Hello HoneyBony",blog:"Hii"},{cat:"ddd",date:"2023",auther:"Bhavya",title:"Hello HoneyBony",blog:"Hii"},{cat:"ddd",date:"2023",auther:"Bhavya",title:"Hello HoneyBony",blog:"Hii"}]
+const blogs=[{cat:"game",date:"2023",author:"Bhavya",title:"HelloHoneyBony",blog:"HelloHoneyBony"},{cat:"ss",date:"2023",author:"Bhv",title:"HelloHoneyBony",blog:"Hii"},{cat:"ddd",date:"2023",author:"Bhav",title:"HHoneyBony",blog:"Hii"},{cat:"ddnr",date:"2023",author:"Bhaa",title:"HeeyBony",blog:"Hii"}]
 
 
 app.get("/ArticleOneBlogs", (req, res) => {
@@ -31,6 +31,10 @@ app.get("/WriteBlog",(req,res)=>{
 
 app.get("/Post",(req,res)=>{
   res.render("Post");
+});
+
+app.get("/Post/:author/:title",(req,res)=>{
+  res.render("Post",{AuthorName:req.params.author,Title:req.params.title});
 });
 // server listening
 app.listen(PORT, () => {
