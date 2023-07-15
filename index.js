d6bfdcbcabf72c1188937834ca83303d8c3a6543
BhavyaMPatel <bhavyapatel4012@gmail.com>
2023-07-15 14:39:43 +0530

    New Update

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
+
 const ejs = require('ejs');
 const authRoutes=require('./routes/authRoute');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
+
 const urlencodedParser = bodyParser.urlencoded({ extended: true })
+
+
 
 mongoose.set('strictQuery',false);
 mongoose.connect("mongodb://127.0.0.1:27017/ArticleOne",{useNewUrlParser: true});
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -22,7 +26,6 @@ app.set("view engine", "ejs");
 // routers  
 app.use(authRoutes)
 // server listening
-
 
 app.listen(PORT, () => {
   console.log(`The app start on http://localhost:${PORT}`);
